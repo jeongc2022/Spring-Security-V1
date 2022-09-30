69364c880f8024732543c47943876cb5c74731f0
jeongc2022 <jeongc2022@gmail.com>
2022-09-30 18:58:12 +0900

    4-3. 시큐리티 회원가입

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller // View를 리턴하겠다!!
 public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	// localhost:8081/
 	// localhost:8081
@@ -32,18 +44,25 @@ public class IndexController {
 	}
 	
 	// 스프링시큐리티 해당주소를 낚아채버리네요!! - SecurityConfig 파일 생성 후 작동 안함.
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료됨!";
+	@PostMapping("/join")
+	public String join(User user) {	// 실제로 회원가입 페이지 연결 (id, password, email)
+		System.out.println(user);
+		user.setRole("ROLE_USER");	// user에 setRole이 없어서 강. id는  auto_increatement, createDate는 @CreationTimestamp때문에 자동으로 만들어짐
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user);	// 회원가입 잘됨. 비밀번호: 1234 => 시큐리티로 로그인 할 수 없음. 이유는 패스워드가 암호화가 안되었기 때문!!
+		return "redirect:/loginForm";	// 회원가입이 정상적으로 완료가 되면 redirect:/loginForm 으로가게 해줌
+													// redirect를 붙이면 loginForm이라는 함수를 호출하게 해줌
 	}
 }
